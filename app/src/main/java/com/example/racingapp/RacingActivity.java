55c9b53ad51909a71c44a3780114f731082b8447
Phan Nguyễn Hoàng Phương <pnhphuong.work1@gmail.com>
2024-09-25 15:43:50 +0700

    added: racing logic and money calculating logic

--- app/src/main/java/com/example/racingapp/RacingActivity.java
+++ app/src/main/java/com/example/racingapp/RacingActivity.java
@@ -1,39 +1,99 @@
 package com.example.racingapp;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.Button;
 import android.widget.SeekBar;
 import androidx.appcompat.app.AppCompatActivity;
-import android.animation.ObjectAnimator;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class RacingActivity extends AppCompatActivity {
     SeekBar horse1, horse2, horse3;
     Button startButton;
+    int[] selectedHorse;
+    int betAmount, betResult = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.racing_view);
+        //Initialize the views
+        Projecting();
+        //Start the race
+        startButton.setOnClickListener(v -> startRace());
+    }
 
+    private void startRace() {
+        horse1.setProgress(0);
+        horse2.setProgress(0);
+        horse3.setProgress(0);
+        final CountDownTimer timer = new CountDownTimer(30000, 300) {
+            @Override
+            public void onTick(long l) {
+                int distanceHorse1 = (int) (Math.random() * 10);
+                int distanceHorse2 = (int) (Math.random() * 10);
+                int distanceHorse3 = (int) (Math.random() * 10);
+                horse1.setProgress(horse1.getProgress() + distanceHorse1);
+                horse2.setProgress(horse2.getProgress() + distanceHorse2);
+                horse3.setProgress(horse3.getProgress() + distanceHorse3);
+
+                if (horse1.getProgress() >= horse1.getMax()) {
+                    int res = finishRace(1);
+                    Toast.makeText(RacingActivity.this
+                            , "Horse 1 wins, "+ res, Toast.LENGTH_SHORT).show();
+                    this.cancel();
+                } else if (horse2.getProgress() >= horse2.getMax()) {
+                    int res = finishRace(2);
+                    Toast.makeText(RacingActivity.this
+                            , "Horse 2 wins, "+ res, Toast.LENGTH_SHORT).show();
+                    this.cancel();
+                } else if (horse3.getProgress() >= horse3.getMax()) {
+                    int res = finishRace(3);
+                    Toast.makeText(RacingActivity.this
+                            , "Horse 3 wins, "+ res, Toast.LENGTH_SHORT).show();
+                    this.cancel();
+                }
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+        timer.start();
+    }
+
+    private int finishRace(int res){
+        if(Arrays.stream(selectedHorse).count() < 2){
+            //Logic for single horse bet
+            if (Arrays.stream(selectedHorse).anyMatch(s -> s == res)){
+                betResult += betAmount;
+            }
+            else{
+                betResult -= betAmount;
+            }
+        }
+        else{
+            //Logic for multiple horses bet
+            long horseCount = Arrays.stream(selectedHorse).count();
+            if (Arrays.stream(selectedHorse).anyMatch(s -> s == res)){
+                betResult = (int)(- betAmount * (horseCount - 2));
+            }
+            else{
+                betResult = (int)(- betAmount * horseCount);
+            }
+        }
+        return betResult;
+    }
+
+    private void Projecting(){
+        //Find the horse views
         horse1 = findViewById(R.id.horse1);
         horse2 = findViewById(R.id.horse2);
         horse3 = findViewById(R.id.horse3);
 
         startButton = findViewById(R.id.btn_start);
-        startButton.setOnClickListener(v -> startRace());
-    }
-
-    private void startRace() {
-        ObjectAnimator animator1 = ObjectAnimator.ofInt(horse1, "progress", 0, 100);
-        animator1.setDuration(5000 + (int)(Math.random() * 3000));
-        animator1.start();
-
-        ObjectAnimator animator2 = ObjectAnimator.ofInt(horse2, "progress", 0, 100);
-        animator2.setDuration(5000 + (int)(Math.random() * 3000));
-        animator2.start();
-
-        ObjectAnimator animator3 = ObjectAnimator.ofInt(horse3, "progress", 0, 100);
-        animator3.setDuration(5000 + (int)(Math.random() * 3000));
-        animator3.start();
     }
 }
